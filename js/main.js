a42f417908b9e4c32bd1ab516e47bb47aeb1ff9a
Rasskazov <rasskazov.paul@yandex.ru>
2020-09-28 23:23:34 +0300

    add operations, and result

--- js/main.js
+++ js/main.js
@@ -11,17 +11,23 @@ class Calculator {
         return this.display;
     }
 
-    printBuffer() {
-        console.log(this.buffer);
-    }
-
     bufferPush(item) {
         this.buffer.push(item);
     }
 
-    getResult() {
+    clearBuffer() {
+        this.buffer = [];
+    }
+
+    getResult(isCals = true) {
         let parsed = this.buffer.join("");
-        return eval(parsed);
+        const result = eval(parsed);
+        this.setInput(result);
+        if(isCals){
+            this.clearBuffer();
+        }
+        this.pushInputToBuffer();
+        return result;
     }
 
     setInput(value) {
@@ -37,24 +43,29 @@ class Calculator {
         this.clearInput();
     }
 
+    reset() {
+        this.clearInput();
+        this.clearBuffer();
+    }
+
+
 }
 
 const calculator = new Calculator();
+const displayTextDOM = document.querySelector(".display__text");
+const operations = ['=', '+', '-', '*', '/', 'ac'];
+
+function addDisplayText(text) {
+    displayTextDOM.textContent += text;
+}
+
+function clearDisplayText() {
+    displayTextDOM.textContent = '';
+}
+
 
 document.addEventListener('DOMContentLoaded', e => {
-    document.querySelector(".display__text").textContent = calculator.getDisplay();
-
-    calculator.printBuffer();
-    calculator.setInput('542');
-    calculator.pushInputToBuffer();
-    calculator.setInput('-');
-    calculator.pushInputToBuffer();
-    calculator.setInput('300');
-    calculator.pushInputToBuffer();
-    calculator.printBuffer();
-
-    console.log(calculator.getResult());
-
+    displayTextDOM.textContent = calculator.getDisplay();
 });
 
 
@@ -62,6 +73,26 @@ const buttons = document.querySelectorAll(".buttons__item");
 
 buttons.forEach(b => {
     b.addEventListener('click', e => {
-        console.log(b.getAttribute('data-action'));
+        const action = b.getAttribute('data-action');
+
+        switch (action) {
+            case "=":
+                clearDisplayText();
+                addDisplayText(calculator.getResult());
+                break;
+            case "ac":
+                clearDisplayText();
+                addDisplayText('0');
+                calculator.reset();
+                break;
+            default:
+                if(displayTextDOM.textContent === '0') {
+                    clearDisplayText();
+                }
+                calculator.setInput(action);
+                addDisplayText(action);
+                calculator.pushInputToBuffer();
+                break;
+        }
     })
 });
